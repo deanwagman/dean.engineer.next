a180763dd336c0e1f787900dbc8b7fcf9fab1de2
Dean Wagman <deanwagman@gmail.com>
2024-10-24 17:04:55 -0700

    Add social meta graph data and assets

--- app/layout.tsx
+++ app/layout.tsx
@@ -20,6 +20,20 @@ export default function RootLayout({
         <head /> will contain the components returned by the nearest parent
         head.tsx. Find out more at https://beta.nextjs.org/docs/api-reference/file-conventions/head
       */}
+      <link
+        rel="icon"
+        type="image/png"
+        href="/favicon-48x48.png"
+        sizes="48x48"
+      />
+      <link rel="icon" type="image/svg+xml" href="/favicon.svg" />
+      <link rel="shortcut icon" href="/favicon.ico" />
+      <link
+        rel="apple-touch-icon"
+        sizes="180x180"
+        href="/apple-touch-icon.png"
+      />
+      <link rel="manifest" href="/site.webmanifest" />
       <head />
       <body>
         {children}
